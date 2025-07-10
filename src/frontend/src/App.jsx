e297caeeda12f6c06a74628b8593da145a77e9bf
Rullred <phamtrunghieudhi1301@gmail.com>
2025-07-10 23:04:18 +0700

    Chỉnh sửa Modal Toast

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css';
 import Input from './components/Input';
 import { Button } from './components/Button';
 import { Card } from './components/Card';
-import { Toast } from './components/Toast';
-
+import {Modal} from './components/Modal';
+import {Toast} from './components/Toast';
 function App() {
   // const [count, setCount] = useState(0);
 
@@ -13,6 +13,7 @@ function App() {
       <Input />
       <Button children={'Đức tuổi'}/>
       <Card/>
+      <Modal/>
       <Toast/>
       <h1>Khởi tạo dự án</h1>
     </>
