faa49963846ffbe98b5ec449683c9435ed55b992
NB4280 <nhlam08096@gmail.com>
2025-07-11 22:20:55 +0700

    Auth-APIs-Register-Login

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,20 @@
+require('dotenv').config(); // Đọc biến từ .env
+
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
 
+const authRoutes = require('./routes/authRoutes'); 
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
