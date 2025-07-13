42419f8b861b7c468aa3cb347faf7387eedd34af
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-07-13 16:01:20 +0700

    MANG-25 feat(api): Create a POST API endpoint in backend and update the init.sql file in the database folder

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const uploadRoutes = require('./routes/uploadRoutes');
+
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./docs/swagger');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+require('dotenv').config();
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
-});
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  })
+);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const postRoutes = require('./routes/postRoutes');
+
+app.use('/api/posts', postRoutes);
+app.use('/api/upload', uploadRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
