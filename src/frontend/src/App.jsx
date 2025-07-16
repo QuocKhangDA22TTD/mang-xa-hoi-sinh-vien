c367b8ec973e382188b86d02fc313279965a7c5e
QuocKhangDA22TTD <quoc.khang.12a4.com>
2025-07-16 12:17:15 +0700

    chore: create feature/newsfeed branch, add HomePage.jsx and update App.jsx

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreatePostPage from './pages/CreatePostPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import HomePage from './layouts/HomePage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<RegisterPage />} />
         <Route path="/register" element={<LoginPage />} />
         <Route path="/create-post" element={<CreatePostPage />} />
+        <Route path="test" element={<HomePage />} />
       </Routes>
     </Router>
   );
