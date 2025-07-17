6b2429156998b98a7bf2cd3284a1113d2296b436
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-07-17 21:12:36 +0700

    chore: update backend/index.js

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,16 +1,19 @@
-require('dotenv').config(); // Đọc biến từ .env
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+
+const cookieParser = require('cookie-parser');
+
 const authRoutes = require('./routes/authRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const chatRoutes = require('./routes/chat.routes');
+
 const app = express();
 
-require('dotenv').config();
-
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -23,11 +26,6 @@ app.use(
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const postRoutes = require('./routes/postRoutes');
-
-app.use('/api/posts', postRoutes);
-app.use('/api/upload', uploadRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/chat', chatRoutes); // Sử dụng chat
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
