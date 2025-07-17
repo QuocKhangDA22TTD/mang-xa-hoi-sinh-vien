6d16bd40906a32dc025d75b8037fb44b2006ab6d
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-07-17 20:47:34 +0700

    MANG-32 feat(ui): create newsfeed page interface

---
+++ src/frontend/src/features/newsfeed/RightBar.jsx
@@ -0,0 +1,12 @@
+function RightBar() {
+  return (
+    <div className="w-[20%] h-[100%] bg-[#F7ECEC] p-4 mx-1 rounded-[3px] border border-[#000000] border-opacity-25 shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]">
+      <div className="flex justify-center items-center w-[100%] h-[10%] bg-[#FDFDFD] border border-[#00A6FB] border-opacity-25 shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]">
+        Bạn bè của bạn đang online
+      </div>
+      <div className="border border-[#000000] border-opacity-25 my-4"></div>
+    </div>
+  );
+}
+
+export default RightBar;
--- src/frontend/src/layouts/HomePage.jsx
+++ src/frontend/src/layouts/HomePage.jsx
@@ -1,15 +1,17 @@
+import Header from '../features/newsfeed/Header';
+import LeftBar from '../features/newsfeed/LeftBar';
+import Content from '../features/newsfeed/Content';
+import RightBar from '../features/newsfeed/RightBar';
+
 function HomePage() {
   return (
     <div className="w-[100%] h-[100vh] bg-gradient-to-b from-[#FFFFFF] to-[#00A6FB]">
-      <div className="w-[100%] h-[10%] bg-gradient-to-r from-[#0582CA] via-[#0295E4] to-[#0582CA] border border-[#000000] border-opacity-25 shadow-[0_4px_8px_4px_rgba(0,0,0,0.25)]"></div>
+      <Header />
       <div className="w-[100%] h-[90%] p-4">
         <div className="flex w-[100%] h-[100%]">
-          <div className="w-[20%] h-[100%] bg-[#F7ECEC] mx-1 rounded-[3px] border border-[#000000] border-opacity-25 shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]"></div>
-          <div className="w-[60%] h-[100%] mx-1 rounded-[3px]">
-            <div className="w-[100%] h-[15%] mb-2 rounded-[3px] bg-gradient-to-r from-[#0582CA] via-[#0295E4] to-[#0582CA] shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]"></div>
-            <div className="w-[100%] h-[83.5%] bg-[#F7ECEC] rounded-[3px] border border-[#000000] border-opacity-25 shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]"></div>
-          </div>
-          <div className="w-[20%] h-[100%] bg-[#F7ECEC] mx-1 rounded-[3px] border border-[#000000] border-opacity-25 shadow-[0_0_8px_4px_rgba(0,0,0,0.25)]"></div>
+          <LeftBar />
+          <Content />
+          <RightBar />
         </div>
       </div>
     </div>
