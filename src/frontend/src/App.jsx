d6226e6a093c45ba0f1be7554054ffc236a9db9a
NB4280 <nhlam08096@gmail.com>
2025-07-17 20:56:00 +0700

    MANG-18 cookie

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,12 +2,24 @@ import './App.css';
 import CreatePostPage from './pages/CreatePostPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+
+import ProtectedRoute from './components/ProtectedRoute'; 
 function App() {
   return (
-    <>
-      {/* <h1>Khởi tạo dự án</h1> */}
-      <LoginPage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/create-post"
+          element={
+            <ProtectedRoute>
+              <CreatePostPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
