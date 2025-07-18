7fcdcb7e096165067399f4718cd84b5c4fa586e4
NB4280 <nhlam08096@gmail.com>
2025-07-18 11:52:44 +0700

    update

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,15 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
-const authRoutes = require("./routes/authRoutes");
-const uploadRoutes = require("./routes/uploadRoutes");
-const chatRoutes = require("./routes/chat.routes");
+const authRoutes = require('./routes/authRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+const chatRoutes = require('./routes/chat.routes');
+const postRoutes = require('./routes/postRoutes');
 
 const app = express();
 
@@ -17,15 +18,17 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
     credentials: true,
   })
 );
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const postRoutes = require("./routes/postRoutes");
+app.use('/api/posts', postRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
