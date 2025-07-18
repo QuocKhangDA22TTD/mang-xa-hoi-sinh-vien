7769ee840a63bebf3afd720dbf055396ce4a84a7
NB4280 <nhlam08096@gmail.com>
2025-07-18 14:32:41 +0700

    MANG-57: Integrate WebSocket

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -6,24 +6,30 @@ import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './layouts/HomePage';
+import ChatPage from './pages/ChatTestPage';
+
+import { SocketProvider } from './context/SocketContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="newsfeed" element={<HomePage />} />
-        <Route
-          path="/create-post"
-          element={
-            <ProtectedRoute>
-              <CreatePostPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <SocketProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/newsfeed" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route
+            path="/create-post"
+            element={
+              <ProtectedRoute>
+                <CreatePostPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </SocketProvider>
   );
 }
 
