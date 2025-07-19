4d10eb902235603c445711d28f8986e4f9afe1ff
Rullred <phamtrunghieudhi1301@gmail.com>
2025-07-19 15:47:38 +0700

    Tạo giao diện CreateAPersonalProfile EditUserProfile PersonalProfile và kết nói API

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -8,6 +8,9 @@ import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './layouts/HomePage';
 import ChatPage from './pages/ChatPage';
 import FriendsPage from './pages/FriendsPage';
+import CreateAPersonalProfile from './pages/CreateAPersonalProfile';
+import EditUserProfile from './pages/EditUserProfile';
+import PersonalProfile from './pages/PersonalProfile';
 
 import { SocketProvider } from './context/SocketContext';
 import { AuthProvider } from './context/AuthContext';
@@ -21,6 +24,9 @@ function App() {
             <Route path="/" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/newsfeed" element={<HomePage />} />
+            <Route path="/create-profile" element={<CreateAPersonalProfile />} />
+            <Route path="/update-profile" element={<EditUserProfile />} />          
+            <Route path="/persona-profile" element={<PersonalProfile />} />
             <Route
               path="/chat"
               element={
