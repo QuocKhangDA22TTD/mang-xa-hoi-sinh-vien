5dea8f6824c5fd5afc7df73b13cafdf9ebb8d8e5
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-07-23 21:59:01 +0700

    update

--- src/frontend/src/pages/RegisterPage.jsx
+++ src/frontend/src/pages/RegisterPage.jsx
@@ -13,13 +13,16 @@ function RegisterPage() {
     }
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const res = await fetch(
+        'http://daring-embrace-production.up.railway.app/api/auth/register',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       if (!res.ok) throw new Error('Đăng ký thất bại');
 
@@ -38,7 +41,9 @@ function RegisterPage() {
       <div className="flex rounded-[2rem] overflow-hidden shadow-2xl w-[45rem] h-[25rem] bg-white">
         {/* Phần form */}
         <div className="flex flex-col flex-1 px-8 py-6">
-          <h1 className="text-center text-2xl font-semibold text-[#0F0F0F] mb-6">ĐĂNG KÝ</h1>
+          <h1 className="text-center text-2xl font-semibold text-[#0F0F0F] mb-6">
+            ĐĂNG KÝ
+          </h1>
 
           <input
             type="email"
@@ -74,7 +79,11 @@ function RegisterPage() {
         <div className="flex flex-col justify-center items-center w-[40%] bg-[#0582CA]">
           <div className="bg-[#EEE] w-[8rem] h-[8rem] rounded-full flex items-center justify-center">
             <div className="text-center font-semibold text-sm">
-              <img src="../../public/demo_avatar.jpg" alt="Logo" className="w-10 h-10 mx-auto mb-1" />
+              <img
+                src="../../public/demo_avatar.jpg"
+                alt="Logo"
+                className="w-10 h-10 mx-auto mb-1"
+              />
               CHÉMNET
             </div>
           </div>
