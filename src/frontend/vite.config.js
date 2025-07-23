80b968f945486af4505fa87eb375b05e4955159b
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2025-07-23 21:18:14 +0700

    update file vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['mang-xa-hoi-sinh-vien-production.up.railway.app'],
+  },
 });
